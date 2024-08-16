44db7c5ef55725fecfb524203e5be7c157065a26
Bishoy-at-pieces <bishoy@pieces.app>
2024-08-16 21:30:37 +0300

    add chat class

---
+++ pieces_copilot_sdk/basic_identifier/chat.py
@@ -0,0 +1,57 @@
+from pieces_os_client.models.conversation import Conversation
+from ..streamed_identifiers import ConversationsSnapshot
+from typing import Optional
+from .basic import Basic
+
+class BasicChat(Basic):
+	def __init__(self,conversation_id) -> None:
+		"""
+		Initialize the Chat with a given Conversation ID.
+
+		:param conversation_id: The ID of the asset.
+		"""
+		self._conversation_id = conversation_id
+		self.conversation:Conversation = ConversationsSnapshot.identifiers_snapshot.get(conversation_id)
+		if not self.conversation:
+			raise ValueError("Conversation not found")
+
+	@property
+	def id(self):
+		return self.conversation.id
+
+	@property
+	def name(self) -> str:
+		return self.conversation.name if self.conversation.name else "New Conversation"
+
+	@name.setter
+	def name(self,name):
+		self.conversation.name = name
+		self._edit_conversation(self.conversation)
+
+	def messages(self) -> List[]:
+		for message_id, index in (self.conversation.messages.indices or {}).items():
+			message_response = self._get_message(message_id)
+
+			if (not message_response.fragment or
+					not message_response.fragment.string or
+					not message_response.fragment.string.raw):
+				continue
+
+
+	@staticmethod
+	def _get_message(message_id):
+		message = ConversationsSnapshot.pieces_client.conversation_message_api.message_specific_message_snapshot(message=message_id,transferables=True)
+		
+
+	@property
+	def annotations(self):
+		return getattr(self.conversation.annotations,"iterable",None)
+
+	def delete(self):
+		ConversationsSnapshot.pieces_client.conversations_api.conversations_delete_specific_conversation(self.id)
+
+	@staticmethod
+	def _edit_conversation(conversation):
+		ConversationsSnapshot.pieces_client.conversation_api.conversation_update(False,conversation)
+
+	
