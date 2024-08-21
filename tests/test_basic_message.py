b12ba096ee3d10d54252f0bcd732f2c1f3804cc5
Sambhav Dixit <94298612+sambhavnoobcoder@users.noreply.github.com>
2024-08-21 09:53:03 +0530

    Create test_basic_message.py
    - basic library imports

---
+++ tests/test_basic_message.py
@@ -0,0 +1,34 @@
+import pytest
+from unittest.mock import Mock, patch, call
+from typing import Literal, Optional, List, TYPE_CHECKING
+from abc import ABC, abstractmethod
+from pieces_os_client import (
+    ApiClient,
+    Application,
+    Configuration,
+    ConversationApi,
+    ConversationMessageApi,
+    ConversationMessagesApi,
+    ConversationsApi,
+    QGPTApi,
+    UserApi,
+    FormatApi,
+    ConnectorApi,
+    SeededConnectorConnection,
+    SeededTrackedApplication,
+    AssetApi,
+    AssetsApi,
+    FragmentMetadata,
+    ModelsApi,
+    AnnotationApi
+)
+from typing import Optional,Dict
+import platform
+import atexit
+import sys
+import importlib.util
+import queue
+from typing import Dict, List, Union, Callable, TYPE_CHECKING
+from pieces_os_client import Conversation, StreamedIdentifiers, Asset
+from abc import ABC, abstractmethod
+import threading
