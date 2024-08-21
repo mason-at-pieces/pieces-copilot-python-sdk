20ff33086c1ab7ce04d0ce698d6198c52d4d02d1
Sambhav Dixit <94298612+sambhavnoobcoder@users.noreply.github.com>
2024-08-22 03:33:47 +0530

    Final refactor
    - Removed redundant imports 
    - Removed import repetitions 
    - Cleaned code

--- tests/test_basic_copilot.py
+++ tests/test_basic_copilot.py
@@ -1,8 +1,7 @@
 import unittest
 from datetime import datetime
 from queue import Queue
-from unittest.mock import Mock, patch
-from typing import Generator
+from unittest.mock import Mock, patch , call
 from pieces_os_client import (
     QGPTStreamOutput,
     RelevantQGPTSeeds,
@@ -11,26 +10,22 @@ from pieces_os_client import (
 )
 from pieces_os_client.models.qgpt_prompt_pipeline import QGPTPromptPipeline
 from typing import TYPE_CHECKING, Optional, Generator
-from pieces_os_client import (SeededConversation,
+import sys
+import importlib.util
+import queue
+from pieces_os_client import Conversation, StreamedIdentifiers, Asset
+from abc import ABC, abstractmethod
+import threading
+import pytest
+from typing import Literal, Optional, List, TYPE_CHECKING ,Generator ,Dict , Union, Callable
+from pieces_os_client import (
+    SeededConversation,
     QGPTStreamInput,
     RelevantQGPTSeeds,
     QGPTQuestionInput,
     QGPTStreamOutput,
     QGPTStreamEnum,
-    QGPTQuestionOutput)
-from pieces_os_client.models.qgpt_prompt_pipeline import QGPTPromptPipeline
-import sys
-import importlib.util
-import queue
-from typing import Literal, Optional, List, TYPE_CHECKING
-from pieces_os_client import Conversation, StreamedIdentifiers, Asset
-from abc import ABC, abstractmethod
-import threading
-import pytest
-from unittest.mock import Mock, patch, call
-from typing import Literal, Optional, List, TYPE_CHECKING
-from abc import ABC, abstractmethod
-from pieces_os_client import (
+    QGPTQuestionOutput,
     ApiClient,
     Application,
     Configuration,
@@ -50,18 +45,8 @@ from pieces_os_client import (
     ModelsApi,
     AnnotationApi
 )
-from typing import Optional,Dict
 import platform
 import atexit
-import sys
-import importlib.util
-import queue
-from typing import Dict, List, Union, Callable, TYPE_CHECKING
-from pieces_os_client import Conversation, StreamedIdentifiers, Asset
-from abc import ABC, abstractmethod
-import threading
-from pieces_os_client import Conversation, StreamedIdentifiers, Asset
-import threading
 from websockets import *
 from pieces_copilot_sdk.copilot import Copilot
 from pieces_copilot_sdk.client import PiecesClient
