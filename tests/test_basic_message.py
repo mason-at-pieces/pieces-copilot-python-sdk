022c67b11aeed020360ce4a0a9cf29b7681d141d
Sambhav Dixit <94298612+sambhavnoobcoder@users.noreply.github.com>
2024-08-21 10:07:20 +0530

    Add pieces_copilot_sdk imports
    this commit adds imports for :
    - Copilot class
    - PiecesClient class

--- tests/test_basic_message.py
+++ tests/test_basic_message.py
@@ -32,3 +32,6 @@ from typing import Dict, List, Union, Callable, TYPE_CHECKING
 from pieces_os_client import Conversation, StreamedIdentifiers, Asset
 from abc import ABC, abstractmethod
 import threading
+from websockets import *
+from pieces_copilot_sdk.copilot import Copilot
+from pieces_copilot_sdk.client import PiecesClient
