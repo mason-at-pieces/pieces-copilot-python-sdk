375ed2d1be6fada2c78d6da0189de6cdcf0c6daa
Sambhav Dixit <94298612+sambhavnoobcoder@users.noreply.github.com>
2024-08-22 02:22:02 +0530

    adding more imports
    - Added imports for support for tests

--- tests/test_basic_copilot.py
+++ tests/test_basic_copilot.py
@@ -21,3 +21,42 @@ from pieces_os_client import (SeededConversation,
 from pieces_os_client.models.qgpt_prompt_pipeline import QGPTPromptPipeline
 import sys
 import importlib.util
+import queue
+from typing import Literal, Optional, List, TYPE_CHECKING
+from pieces_os_client import Conversation, StreamedIdentifiers, Asset
+from abc import ABC, abstractmethod
+import threading
+import pytest
+from unittest.mock import Mock, patch, call
+from typing import Literal, Optional, List, TYPE_CHECKING
+from abc import ABC, abstractmethod
+from pieces_os_client import (
+    ApiClient,
+    Application,
+    Configuration,
+    ConversationApi,
+    ConversationMessageApi,
+    ConversationMessagesApi,
+    ConversationsApi,
+    QGPTApi,
+    UserApi,
+    FormatApi,
+    ConnectorApi,
+    SeededConnectorConnection,
+    SeededTrackedApplication,
+    AssetApi,
+    AssetsApi,
+    FragmentMetadata,
+    ModelsApi,
+    AnnotationApi
+)
+from typing import Optional,Dict
+import platform
+import atexit
+import sys
+import importlib.util
+import queue
+from typing import Dict, List, Union, Callable, TYPE_CHECKING
+from pieces_os_client import Conversation, StreamedIdentifiers, Asset
+from abc import ABC, abstractmethod
+import threading
